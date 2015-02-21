7911d9b002be389365cbce14e488ea464e181520
Kristof Mattei <github@kristofmattei.be>
2015-02-21 16:47:31 +0100

    Code that actually should work.

--- boto-s3-encryption/main.py
+++ boto-s3-encryption/main.py
@@ -1,17 +1,30 @@
-from boto.s3.connection import S3Connection
+import boto
+from boto import s3
+from boto.s3.key import Key
+from boto.s3.connection import Location
+import json
 import time
 
-connection = S3Connection("eu-west-1")
-bucket_name = "encryption-test-%s" % time.time()
+if not boto.config.get('s3', 'use-sigv4'):
+    boto.config.add_section('s3')
+    boto.config.set('s3', 'use-sigv4', 'True')
+
+
+bucket_name = "encryption-test-%s" % int(time.time())
+
+connection = s3.connect_to_region("eu-west-1")
+
 
 def create_or_get_bucket(connection, policy):
     bucket = None
     try:
         bucket = connection.get_bucket(bucket_name)
     except:
-        bucket = connection.create_bucket(bucket_name)
+        bucket = connection.create_bucket(bucket_name, location=Location.EU)
 
     bucket.set_policy(json.dumps(policy))
+
+    return bucket
 
 policy = {
     "Version":"2012-10-17",
@@ -33,11 +46,17 @@ policy = {
 bucket = create_or_get_bucket(connection, policy)
 
 
+
 headers = {
     "x-amz-server-side-encryption": "aws:kms",
-    "x-amz-server-side-encryption-aws-kms-key-id": "ARN GOES HERE"
+    #"x-amz-server-side-encryption-aws-kms-key-id": "zzzzz"
 }
-#
-# key = Key(bucket, "foobar")
-# key.key = "what?"
-# key.set_contents_from_string()
+
+key = Key(bucket, "foobar")
+key.set_contents_from_string("foobar", headers=headers)
+# this line throws because of 
+# https://github.com/boto/boto/blob/develop/boto/s3/key.py#L980
+
+
+
+bucket.delete()
